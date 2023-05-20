a01efb559d82d99e16221cebc4af851c628767ce
Andresa Alves <andresa_15ga@hotmail.com>
2023-05-19 21:41:44 -0300

    Começando front, mobile e back

---
+++ server/src/server.ts
@@ -0,0 +1,30 @@
+import 'dotenv/config'
+
+import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { authRoutes } from './routes/auth'
+
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Running')
+  })
